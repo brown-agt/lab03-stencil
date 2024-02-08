49b75621184133eb0d9a59d09f29efd954a1aee3
John Wu <john_w_wu@brown.edu>
2024-02-08 17:54:20 -0500

    DRAFT 1

---
+++ README.md
@@ -0,0 +1,45 @@
+# CS1440/2440 Lab 3: QLearning and Collusion
+
+## Introduction
+
+## Setup and Installation
+Follow these steps to set up your environment and install the necessary package for the lab.
+
+### Step 1: Git Clone the Repository 
+Open your terminal and navigate to where you want to clone the repository
+```bash 
+git clone https://github.com/brown-agt/lab03-stencil.git
+```
+
+### Step 2: Create a Virtual Environment
+Please then navigate to your project directory. Run the following commands to create a Python virtual environment named `.venv`.
+
+If you own a Mac 
+```bash
+python -m venv .venv
+source .venv/bin/activate
+```
+
+If you own a Windows 
+```bash 
+python -m venv .venv
+.venv\Scripts\activate
+```
+
+### Step 3: Install the agt server package
+```bash
+pip install --upgrade pip
+pip install agt-server
+```
+
+## Agent Methods 
+For the `ChickenAgent`s here are a few methods that you may find helpful! 
+- `self.calculate_utils(a1, a2)` is a method that takes in player 1's action (`a1`) and player 2's action (`a2`) and returns a list [`u1`, `u2`] where `u1` is player1's utility and `u2` is player 2's utility. 
+- `self.get_action_history()` is a method that returns a list of your actions from previous rounds played.
+- `self.get_util_history()` is a method that returns a list of your utility from previous rounds played. 
+- `self.get_opp_action_history()` is a method that returns a list of your opponent's actions from previous rounds played.
+- `self.get_opp_util_history()` is a method that returns a list of your opponent's utility from previous rounds played.
+- `self.get_last_action()` is a method that returns a your last action from the previous round.
+- `self.get_last_util()` is a method that returns a your last utility from the previous round.
+- `self.get_opp_action_history()` is a method that returns a your opponent's last action from the previous round.
+- `self.get_opp_util_history()` is a method that returns a your opponent's last utility from the previous round.
---
+++ my_agent.py
@@ -0,0 +1,83 @@
+from q_learning import QLearning
+from agt_server.local_games.chicken_arena import ChickenArena
+from agt_server.agents.test_agents.chicken.mystery_agent.my_agent import MysteryAgent
+
+
+NUM_TRAINING_ITERATIONS = 20000
+NUM_ITERATIONS_PER_PRINT = 1000
+
+
+class MyChickenAgent(QLearning):
+    def __init__(self, name, num_possible_states, num_possible_actions, initial_state, learning_rate, discount_factor, exploration_rate, training_mode, save_path=None) -> None:
+        super().__init__(name, num_possible_states, num_possible_actions, initial_state,
+                         learning_rate, discount_factor, exploration_rate, training_mode, save_path)
+        self.name = "TODO: NAME YOUR AGENT"
+
+    def determine_state(self):
+        # Determines the next state s_prime given the action histories and reward histories
+        my_action_hist = self.get_action_history()
+        opp_action_hist = self.get_opp_action_history()
+        my_util_hist = self.get_util_history()
+        
+        # TODO: Fill out this function
+        raise NotImplementedError
+
+
+if __name__ == "__main__":
+    NUM_TRAINING_ITERATIONS = 20000
+    NUM_ITERATIONS_PER_PRINT = 1000
+    # this agent only uses ??? states
+    NUM_POSSIBLE_STATES = ???  # TODO: UPDATE THE NUMBER OF POSSIBLE STATES
+    # chicken has 2 possible actions (CONTINUE and SWERVE).
+    NUM_POSSIBLE_ACTIONS = 2
+
+    # Feel Free to change any of these parameters!
+    INITIAL_STATE = 0
+    LEARNING_RATE = 0.05
+    DISCOUNT_FACTOR = 0.90
+    EXPLORATION_RATE = 0.05
+    
+    # TODO: Please fill out each of these parameters: 
+    name = ... # Give your agent a name 
+    train = True # Set this to false if you want your agent to be purely exploiting and not exploring after training 
+    save_q_table = "qtable.npy" # Set to None to not save the q-table, otherwise save it under the file path that you give it E.g. "qtable.npy" 
+                        # If the file already exists then it will initialize the q-table using the saved npy file. 
+                        # TODO: PLEASE SAVE IT AS AN ACTUAL FILE like "qtable.npy" SO YOU CAN TEST IT
+    join_server = False
+    ip = ... 
+    port = ...
+
+    # START SIMULATING THE GAME
+    agent = MyChickenAgent(name, NUM_POSSIBLE_STATES, NUM_POSSIBLE_ACTIONS,
+                            INITIAL_STATE, LEARNING_RATE, DISCOUNT_FACTOR, EXPLORATION_RATE, train, save_q_table)
+    
+    if join_server: 
+        agent.connect(ip=args.ip, port=args.port)
+    else: 
+        if train: 
+            print("TRAINING PERFORMANCE")
+            arena = ChickenArena(
+                num_rounds=10000,
+                timeout=1,
+                players=[
+                    agent,
+                    MysteryAgent("Bok"),
+                    MysteryAgent("Cluck"),
+                    MysteryAgent("Buck"),
+                    MysteryAgent("Bugawk")
+                ]
+            )
+            arena.run()
+        print("TESTING PERFORMANCE")
+        arena = ChickenArena(
+            num_rounds=1000,
+            timeout=1,
+            players=[
+                agent,
+                MysteryAgent("Bok"),
+                MysteryAgent("Cluck"),
+                MysteryAgent("Buck"),
+                MysteryAgent("Bugawk")
+            ]
+        )
+        arena.run()
